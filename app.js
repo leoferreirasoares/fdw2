f593693e9e4cf3f2cae6e39825c6d878b65a1b78
leoferreirasoares <leoferreirasoares@gmail.com>
2022-09-22 19:54:01 -0300

    exercicio 2

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ app.get('/',(req,res)=>{
 app.get('/produtos',(req,res)=>{
     res.send('Listagem de produtos!')
 })
+app.post('/produtos',(req,res)=>{
+    res.send('cadastrando produtos!')
+})
+app.get('/produtos/:id',(req,res)=>{
+    res.send('Buscando o produto'+req.params.id)
+})
+app.put('/produtos/:id',(req,res)=>{
+    res.send('Atualizando o produto'+req.params.id)
+})
+app.delete('/produtos/:id',(req,res)=>{
+    res.send('Deletando o produto'+req.params.id)
+})
 app.listen(port,()=>{
     console.log('example app listening on port ${port}')
 })
